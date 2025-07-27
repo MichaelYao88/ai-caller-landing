e947c0731728147ba130732d61ab3baaa36d489c
Michael Yao <Michaelyao88@gmail.com>
2025-07-27 19:30:49 +0500

    module exports

--- api/verify-recaptcha.js
+++ api/verify-recaptcha.js
@@ -1,29 +1,29 @@
-export default async function handler(req, res) {
-    if (req.method !== "POST") {
-      return res.status(405).json({ message: "Method Not Allowed" });
-    }
-  
-    const { token } = req.body;
-    const secret = process.env.RECAPTCHA_SECRET_KEY;
-  
-    if (!token) {
-      return res.status(400).json({ success: false, message: "Missing token" });
-    }
-  
-    try {
-      const secret = process.env.RECAPTCHA_SECRET_KEY;
-      const response = await fetch(
-        `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${token}`,
-        {
-          method: "POST",
-        }
-      );
-  
-      const data = await response.json();
-      return res.status(200).json({ success: data.success });
-    } catch (error) {
-      console.error("Error verifying reCAPTCHA:", error);
-      return res.status(500).json({ success: false });
-    }
+module.exports = async (req, res) => {
+  if (req.method !== "POST") {
+    return res.status(405).json({ message: "Method Not Allowed" });
   }
-  
+
+  const { token } = req.body;
+  const secret = process.env.RECAPTCHA_SECRET_KEY;
+
+  if (!token) {
+    return res.status(400).json({ success: false, message: "Missing token" });
+  }
+
+  try {
+    const secret = process.env.RECAPTCHA_SECRET_KEY;
+    const response = await fetch(
+      `https://www.google.com/recaptcha/api/siteverify?secret=${secret}&response=${token}`,
+      {
+        method: "POST",
+      }
+    );
+
+    const data = await response.json();
+    return res.status(200).json({ success: data.success });
+  } catch (error) {
+    console.error("Error verifying reCAPTCHA:", error);
+    return res.status(500).json({ success: false });
+  }
+};
+
