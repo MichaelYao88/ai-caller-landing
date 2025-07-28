1bb7a003bfeeb7655e5045150d0d645f7f57e82e
Michael Yao <Michaelyao88@gmail.com>
2025-07-28 16:24:32 +0500

    redeploy firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,20 @@
-// src/firebase.js
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Your Firebase config
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA2DaNwp0H7fhRantPaSlU6APbzEftSv-8",
-  authDomain: "appointmentcaller-3b3d2.firebaseapp.com",
-  projectId: "appointmentcaller-3b3d2",
-  storageBucket: "appointmentcaller-3b3d2.firebasestorage.app",
-  messagingSenderId: "248148065520",
-  appId: "1:248148065520:web:6ba5df760edba6ca609635"
+  // Add your Firebase config here
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
